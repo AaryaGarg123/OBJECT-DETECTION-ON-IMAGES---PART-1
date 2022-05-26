1bf1470069bde1c34c28e909a7f97b004a53c5fa
AaryaGarg123 <90965186+AaryaGarg123@users.noreply.github.com>
2022-05-26 12:05:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function BedRoom(){
+    window.location = bedroom.html;
+}
+
+function tv() {
+    window.location = tv.html;
+}
+
+function desk() {
+    window.location = desk.html;
+}
+
+function bottles() {
+    window.location = bottles.html;
+}
+
+function fruit() {
+    window.location = fruit.html;
+}
